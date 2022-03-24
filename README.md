0223b71cf0b9e37f69bbf83f290bf9fa5799d369
Ruh Aim <ruhaim@gmail.com>
2022-03-24 17:08:09 +0530

    docs: update readme and some jsDocs

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # iprice-test
+
+## Setup development env
+1. Run `yarn install` on repo root
+## Run instructions
+1. Run `yarn start <...args>` on the repo root
+eg: `yarn start hello world`
+
+## Run unit tests
+1. Run `yarn test` on the repo root
+
+## To add new text output strategies
+1. Define a new class and extend it with `IOutputStrategy`
+2. Override the `output()` function and write custom strategy code. Return the text that needs to be printed in the console.
+3. Pass an instance of the new strategy as an array to the TextOutputter constructor,
+eg: If your strategy was `LowerCaseOutput`, you can pass to TextOutputter as follows 
+`new TextOutputter([new LowerCaseOutput()])`
+see `index.js` in `src` folder for working example.
+
--- src/TextExtractor.js
+++ src/TextExtractor.js
@@ -1,12 +1,18 @@
+/**
+ * Class that holds the function to read the inputText via CLI args
+ */
 export class TextExtractor {
-    
-    static extractCliArg() {
-      const [, , ...inputParams] = process.argv;
-      if (!inputParams.length) {
-        throw new Error("No input text found, please try again with valid text");
-      }
-      return inputParams.join(" ");
+  /**
+   * extracts the input text from the commandline arguments
+   * used when executing the script
+   * @return {void} - Logs an error for each strategry that fails
+   * @throws {Error} if no argument is passed
+   */
+  static extractCliArg() {
+    const [, , ...inputParams] = process.argv;
+    if (!inputParams.length) {
+      throw new Error("No input text found, please try again with valid text");
     }
-  
-
+    return inputParams.join(" ");
   }
+}
